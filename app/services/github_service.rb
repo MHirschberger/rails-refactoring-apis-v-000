eb82277d71abc55fc7f68906dd1f8e06ed8ea0f9
Michael Hirschberger <mlhirschberger@gmail.com>
2018-08-01 20:58:19 -0400

    Done.

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,6 +1,6 @@
 class GithubService
 
-  attr_reader :access_token
+  attr_accessor :access_token
 
   def initialize(access_hash = nil)
     @access_token = access_hash["access_token"] if access_hash
@@ -10,8 +10,10 @@ class GithubService
     response = Faraday.post "https://github.com/login/oauth/access_token",
         {client_id: client_id, client_secret: client_secret, code: code},
         {'Accept' => 'application/json'}
+
     access_hash = JSON.parse(response.body)
-    @access_token = access_hash["access_token"]
+    self.access_token = access_hash["access_token"]
+    self.access_token
   end
 
   def get_username
@@ -23,7 +25,7 @@ class GithubService
   def get_repos
     response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
     repos_array = JSON.parse(response.body)
-    repos_array.map{|repo| GithubRepo.new(repo) }
+    repos_array.map{ |repo| GithubRepo.new(repo) }
   end
 
   def create_repo(name)
